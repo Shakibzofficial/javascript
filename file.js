90c1637a6eefb1e462a50615954f1ed3ee465ee5
Your Name <you@example.com>
2021-03-07 23:06:24 +0600

    complete

---
+++ file.js
@@ -0,0 +1,62 @@
+console.log(10 + 20)
+
+var DigitalMarketing= 5000;
+var GraphicDesign= 10000;
+var WebDesign=8000;
+
+console.log(DigitalMarketing + GraphicDesign + WebDesign)
+
+var studentFName="Shakib";
+var studentLName="Khan";
+var fullName= (studentFName +" " + studentLName)
+console.log(fullName)
+
+var laptopBrand="Asus";
+var mouseBrand="Walton";
+var KeyboardBrand="Logic"
+var laptopPrice=20500.70;
+var mouseprice=250.40;
+var keyboardPrice=480;
+
+var BrandNames=("Laptop-Name: "+laptopBrand + "|" + " " + "Mouse-Name: "+ mouseBrand + "| " 
++ "KeyBoard-Name " + KeyboardBrand);
+
+console.log(BrandNames);
+
+var LaptopFinalPrice= parseInt(laptopPrice);
+var MouseFinalPrice= parseInt(mouseprice);
+var KeyBoardFinalPrice= parseInt(keyboardPrice);
+
+var totalPrice = (LaptopFinalPrice + MouseFinalPrice + KeyBoardFinalPrice)
+
+console.log(totalPrice)
+
+
+var StudentID= 57494;
+var StudentFName="Shakib"
+var StudentLname="Khan"
+
+var Final= (StudentID + ": " + StudentFName + "-" + StudentLname);
+console.log(Final)
+
+
+var price= 400;
+var TotalPrice= price += 120;
+
+console.log(TotalPrice)
+
+var ProductOnePrice= 500;
+var ProductTwoPrice= 700;
+
+var PriceCheck =  ProductOnePrice == ProductTwoPrice;
+
+console.log(PriceCheck)
+
+var one= 100;
+var two= "500";
+
+var check= one != two;
+
+console.log(check)
+
+
